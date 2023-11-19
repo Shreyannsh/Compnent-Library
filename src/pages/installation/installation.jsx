370f38f9058b7d1032dad908aacb5f5316eb1261
Shreyansh Tiwari <111145568+Shreyannsh@users.noreply.github.com>
2023-11-19 20:25:03 +0530

    refactor

--- src/pages/installation/installation.jsx
+++ src/pages/installation/installation.jsx
@@ -19,7 +19,7 @@ const Installation = () => {
       </p>
 
       <iframe
-        src="https://carbon.now.sh/embed?bg=rgba%28228%2C228%2C228%2C1%29&t=vscode&wt=none&l=javascript&width=871&ds=true&dsyoff=20px&dsblur=52px&wc=true&wa=false&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=14px&lh=90%25&si=false&es=2x&wm=false&code=%2520%2520%2520%2520%253Clink%2520rel%253D%2522stylesheet%2522%2520href%253D%2522https%253A%252F%252Felementhub.netlify.app%252Fstyle%252Fmain.css%2522%2520%252F%253E%250A"
+        src="https://carbon.now.sh/embed?bg=rgba%28228%2C228%2C228%2C1%29&t=vscode&wt=none&l=javascript&width=871&ds=true&dsyoff=20px&dsblur=52px&wc=true&wa=false&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=13.5px&lh=90%25&si=false&es=2x&wm=false&code=%2520%2520%253Clink%2520rel%253D%2522stylesheet%2522%2520href%253D%2522https%253A%252F%252Fcompnent-library.vercel.app%252Fstyle%252Fmain.css%2522%2520%252F%253E%250A%250A"
         className="iframe"
         sandbox="allow-scripts allow-same-origin"
       ></iframe>
@@ -27,12 +27,10 @@ const Installation = () => {
       <p>Also you can import to your stylesheets by import link</p>
 
       <iframe
-        src="https://carbon.now.sh/embed?bg=rgba%28228%2C228%2C228%2C1%29&t=vscode&wt=none&l=javascript&width=871&ds=true&dsyoff=20px&dsblur=52px&wc=true&wa=false&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=13.5px&lh=90%25&si=false&es=2x&wm=false&code=%2509%2509%2509%2509%2509%2540import%2520url%28%27https%253A%252F%252Felementhub.netlify.app%252Fstyle%252Fmain.css%27%29%250A%250A"
+        src="https://carbon.now.sh/embed?bg=rgba%28228%2C228%2C228%2C1%29&t=vscode&wt=none&l=javascript&width=871&ds=true&dsyoff=20px&dsblur=52px&wc=true&wa=false&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=13.5px&lh=90%25&si=false&es=2x&wm=false&code=%2509%2509%2509%2509%2509%2540import%2520url%28%27https%253A%252F%252Fcompnent-library.vercel.app%252Fstyle%252Fmain.css%27%29%250A%250A"
         className="iframe"
-        //style="width: 871px; height: 93px; border:0; transform: scale(1); overflow:hidden;"
         sandbox="allow-scripts allow-same-origin"
       ></iframe>
-
       <h3>Using EmelmentHub style to your web pages</h3>
       <p>
         After linking to your style you can simply write the class or className
